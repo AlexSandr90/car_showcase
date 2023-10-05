6d7a3d0a07f9969630893279af0bf093c3f27e72
AlexSandr90 <gayrsuk@gmail.com>
2023-10-05 16:06:39 +0300

    added Rapid Api cars, card details modal

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
-import { CustomFilter, Hero, SearchBar } from '@/components';
+import { CarCard, CustomFilter, Hero, SearchBar } from '@/components';
+import { fetchCars } from '@/utils';
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+  console.log(allCars);
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
 
       <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
-          <h1 className="text-4x1 font-extrabold">Car Cataloque1</h1>
+          <h1 className="text-4x1 font-extrabold">Car Cataloque</h1>
           <p>Explore the cars you might like</p>
         </div>
 
@@ -15,10 +20,26 @@ export default function Home() {
           <SearchBar />
 
           <div className="home__filter-container">
-            <CustomFilter title='fuel' />
-            <CustomFilter title='year' />
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard key={car} car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
--- components/index.ts
+++ components/index.ts
@@ -5,13 +5,15 @@ import Footer from './Footer';
 import SearchBar from './SearchBar';
 import CustomFilter from './CustomFilter';
 import SearchManufacturer from './SearchManufacturer';
+import CarCard from './CarCard';
 
 export {
   Hero,
   Navbar,
   Footer,
+  CarCard,
   SearchBar,
   CustomFilter,
-  CustomButton,
+  CustomButton as CustomButton,
   SearchManufacturer,
 };
